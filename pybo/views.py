0607fc7e2112f17f9747b68ac26df31703886293
HyeokJu <qkrgurwn1231@gmail.com>
2022-08-30 15:31:59 +0900

    페이지와 로그인, 로그아웃 기능 추가
    pagination, common

--- pybo/views.py
+++ pybo/views.py
@@ -4,12 +4,18 @@ from .models import Question
 from .forms import QuestionForm
 from django.http import HttpResponseNotAllowed
 from .forms import AnswerForm
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
+    # 페이지
+    page = request.GET.get('page', '1')
     # order_by(): 괄호 안의 기준으로 정렬하여 반환하는 함수
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    # 페이지 당 10의 데이터 보여주기
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
     # render: 파이썬 데이터를 템플릿에 적용하여 html로 반환하는 함수
     # 탬플릿: pybo_question_list.html
     return render(request, 'pybo/question_list.html', context)
